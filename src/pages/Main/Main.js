880d4c1ed020165218c1c7b953771e7119dad0c9
Suhyun_Lim <suhuyn19@naver.com>
2020-11-19 13:42:37 +0900

    Side 기능 구현 중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Main.scss";
 
 const Main = () => {
-  return <div>asd</div>;
+  return (
+    <>
+      <div></div>;
+    </>
+  );
 };
 
 export default Main;
