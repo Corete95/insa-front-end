aac507424a5d518596168f8e5ca90d4e515bdfc4
Suhyun_Lim <suhuyn19@naver.com>
2020-11-23 18:06:46 +0900

    Main page 작업 시작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Nav from "../../components/Nav/Nav";
+import Side from "../../components/Side/Side";
 import "./Main.scss";
 
 const Main = () => {
   return (
     <>
-      <div></div>;
+      {/* <Nav />
+      <Side /> */}
+      <div>
+        <span>Notice</span>
+        <ul>
+          <li>
+            <span>여기는 제목이 옵니다...</span>
+            <p>여기는 본문입니다...</p>
+            <span>여기는 날짜입니다.</span>
+          </li>
+          <li>
+            <span>여기는 제목이 옵니다...</span>
+            <p>여기는 본문입니다...</p>
+            <span>여기는 날짜입니다.</span>
+          </li>
+          <li>
+            <span>여기는 제목이 옵니다...</span>
+            <p>여기는 본문입니다...</p>
+            <span>여기는 날짜입니다.</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <span>Project</span>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </>
   );
 };
