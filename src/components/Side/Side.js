459a8e08301566c9d08f4bcb294002e8011ffc3b
Suhyun_Lim <suhuyn19@naver.com>
2020-12-01 14:13:53 +0900

    타이머: 일시정지, 재시작 기능 작업 중

--- src/components/Side/Side.js
+++ src/components/Side/Side.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Calendar from "../../components/Side/components/react-calendar/src/Calendar";
-import Timer from "react-compound-timer";
+import axios from "axios";
 import { BsPlus } from "react-icons/bs";
 import "./Side.scss";
 
@@ -10,103 +10,146 @@ const Side = () => {
   const [hiddenState, setHiddenState] = useState(false);
   const [userState, setUserState] = useState("근무 중");
 
+  const [onWorking, setOnWorking] = useState(false);
+  const [startTime, setStartTime] = useState(null);
+  const [nowTime, setNowTime] = useState(null);
+  const [pauseTime, setPauseTime] = useState(null);
+  const [reStartTime, setReStartTime] = useState(null);
+  const [workingHours, setWorkingHours] = useState(new Date(0));
+
   const stateName = ["근무 중", "식사", "휴식"];
 
   const showingButton = () => {
     setHiddenState(!hiddenState);
   };
 
+  let interval;
+
+  const timer = () => {
+    if (!startTime) {
+      setStartTime(new Date());
+      setOnWorking(true);
+    } else if (startTime && pauseTime) {
+      let plusTime = reStartTime - pauseTime;
+      console.log(typeof plusTime);
+      new Date(plusTime);
+      console.log(typeof plusTime);
+      setStartTime(new Date(startTime + plusTime));
+    }
+    console.log("시작 시간", startTime);
+    console.log(typeof nowTime);
+    console.log(typeof startTime);
+    setNowTime(new Date());
+    setWorkingHours(new Date(nowTime - startTime));
+  };
+
+  useEffect(() => {
+    if (onWorking) {
+      interval = setInterval(() => timer(), 1000);
+    }
+    return () => clearInterval(interval);
+  }, [workingHours, onWorking]);
+
+  const resumeTimer = (e) => {
+    if (workingHours.getTime() > 0) {
+      setUserState(e.target.value);
+      setOnWorking(true);
+      setReStartTime(new Date());
+    }
+  };
+
+  const stopTimer = (e) => {
+    if (workingHours.getTime() > 0) {
+      setUserState(e.target.value);
+      setOnWorking(false);
+      setPauseTime(new Date());
+      clearTimeout(interval);
+    }
+  };
+
+  const finishTimer = () => {
+    setOnWorking(false);
+    setStartTime(null);
+    setWorkingHours(new Date(0));
+    clearTimeout(interval);
+  };
+
   return (
     <SideBarContainer>
-      <Timer
-        initialTime={0}
-        startImmediately={false}
-        onStart={() => {}}
-        onResume={() => {}}
-        onPause={() => {}}
-        onStop={() => {}}
-        onReset={() => {}}
-      >
-        {({ start, resume, pause, stop, reset }) => (
-          <React.Fragment>
-            <Profile>
-              <div className="profileContainer">
-                <img
-                  src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80"
-                  alt="예시 입니다."
-                />
-                <BsPlus className="plusButton" />
-              </div>
-              <div className="userData">
-                <div className="userState">{userState}</div>
-                <div className="userName">
-                  김인사
-                  <StateChangeButton onClick={showingButton}>
-                    ▼
-                  </StateChangeButton>
-                </div>
-                <div className="hiddenContainer">
-                  {stateName.map((state, idx) => {
-                    return (
-                      <>
-                        {idx === 0 && (
-                          <HiddenButton
-                            idx={idx}
-                            hiddenState={hiddenState}
-                            value={state}
-                            onClick={(e) => {
-                              setUserState(e.target.value);
-                              resume();
-                            }}
-                          >
-                            {state}
-                          </HiddenButton>
-                        )}
-                        {idx !== 0 && (
-                          <HiddenButton
-                            idx={idx}
-                            hiddenState={hiddenState}
-                            value={state}
-                            onClick={(e) => {
-                              setUserState(e.target.value);
-                              pause();
-                            }}
-                          >
-                            {state}
-                          </HiddenButton>
-                        )}
-                      </>
-                    );
-                  })}
-                </div>
-                <div className="dDay">D +555</div>
-                <div>
-                  <Timer.Hours /> 시간
-                  <Timer.Minutes /> 분
-                  <Timer.Seconds /> 초
-                </div>
-              </div>
-            </Profile>
-            <Calendar onChange={onChange} value={value} />
-            <WorkingState>
-              <div>
-                <button className="belowButton" onClick={start}>
-                  출근하기
-                </button>
-                <button
-                  className="belowButton"
-                  onClick={() => {
-                    stop();
-                    reset();
-                  }}
-                >
-                  퇴근하기
-                </button>
-              </div>
-            </WorkingState>
-          </React.Fragment>
-        )}
-      </Timer>
+      <Profile>
+        <div className="profileContainer">
+          <img
+            src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80"
+            alt="예시 입니다."
+          />
+          <BsPlus className="plusButton" />
+        </div>
+        <div className="userData">
+          <div className="userState">{userState}</div>
+          <div className="userName">
+            김인사
+            <StateChangeButton onClick={showingButton}>▼</StateChangeButton>
+          </div>
+          <div className="hiddenContainer">
+            {stateName.map((state, idx) => {
+              return (
+                <>
+                  {idx === 0 && (
+                    <HiddenButton
+                      idx={idx}
+                      hiddenState={hiddenState}
+                      value={state}
+                      onClick={(e) => {
+                        resumeTimer(e);
+                      }}
+                    >
+                      {state}
+                    </HiddenButton>
+                  )}
+                  {idx !== 0 && (
+                    <HiddenButton
+                      idx={idx}
+                      hiddenState={hiddenState}
+                      value={state}
+                      onClick={(e) => {
+                        stopTimer(e);
+                      }}
+                    >
+                      {state}
+                    </HiddenButton>
+                  )}
+                </>
+              );
+            })}
+          </div>
+          <div className="dDay">D +555</div>
+        </div>
+        <div>
+          {workingHours.getHours() - 9}시간 {workingHours.getMinutes()}분{" "}
+          {workingHours.getSeconds()}초
+        </div>
+      </Profile>
+      <Calendar onChange={onChange} value={value} />
+      <WorkingState>
+        <div>
+          <button
+            className="belowButton"
+            onClick={() => {
+              timer();
+            }}
+          >
+            출근하기
+          </button>
+          <button
+            className="belowButton"
+            onClick={() => {
+              finishTimer();
+            }}
+          >
+            퇴근하기
+          </button>
+        </div>
+      </WorkingState>
     </SideBarContainer>
   );
 };
@@ -117,18 +160,18 @@ const SideBarContainer = styled.aside`
   position: fixed;
   text-align: center;
   width: 315px;
-  height: 100vh;
+  height: calc(100% - 90px);
   background-color: rgba(34, 34, 34, 0.5);
   z-index: -1;
 `;
 
 const Profile = styled.div`
-  margin: 140px 0px 90px 0px;
+  margin: 80px 0px 60px 0px;
   color: #ffffff;
 
   .profileContainer {
     width: 100%;
-    height: 19vh;
+    height: 161px;
 
     img {
       width: 51%;
@@ -138,7 +181,7 @@ const Profile = styled.div`
 
     .plusButton {
       position: absolute;
-      top: 265px;
+      top: 205px;
       left: 203px;
       width: 28px;
       height: 28px;
@@ -179,7 +222,7 @@ const Profile = styled.div`
 
     .dDay {
       font-size: 36px;
-      margin-bottom: 15px;
+      margin-bottom: 50px;
     }
   }
 `;
