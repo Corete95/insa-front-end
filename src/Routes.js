59087effbd79c806474b46b6e3f311685206b9e3
Corete <fanthell95@gmail.com>
2020-12-07 15:51:58 +0900

    프로젝트 디테일 레이아웃+1차 기능 완성

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import SignupEnd from "./pages/Login/SignupEnd";
 import Main from "./pages/Main/Main";
 import Notice from "./pages/Notice/Notice";
 import NoticeWriting from "./pages/Notice/NoticeWriting";
 import Project from "./pages/Project/Project";
+import ProjectDetail from "./pages/Project/ProjectDetail";
 import EditUserInfo from "./pages/EditUserInfo/EditUserInfo";
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
@@ -30,6 +30,11 @@ class Routes extends Component {
                   <Route exact path="/Main" component={Main} />
                   <Route exact path="/Notice" component={Notice} />
                   <Route exact path="/Project" component={Project} />
+                  <Route
+                    exact
+                    path="/ProjectDetail"
+                    component={ProjectDetail}
+                  />
                   <Route
                     exact
                     path="/NoticeWriting"
